bbc06e74239b4b3979fb59a11b8cd161261d1dba
D.L <dl@users.noreply.github.com>
2018-12-17 19:32:30 +0800

    nodejs basic sample

--- fileUpload/server.js
+++ fileUpload/server.js
@@ -1,44 +1,13 @@
-﻿var express = require('express');
-var app = express();
-var fs = require("fs");
+﻿var jqupload = require('jquery-file-upload-middleware');
 
-var bodyParser = require('body-parser');
-var multer = require('multer');
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/' }).array('image'));
-
-app.get('/fileUpload.html', function (req, res) {
-    res.sendFile(__dirname + "/" + "fileUpload.html");
-})
-
-app.post('/file_upload', function (req, res) {
-
-    console.log(req.files[0]);  
-
-    var des_file = __dirname + "/" + req.files[0].originalname;
-    fs.readFile(req.files[0].path, function (err, data) {
-        fs.writeFile(des_file, data, function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                response = {
-                    message: 'file uploaded successfully',
-                    filename: req.files[0].originalname
-                };
-            }
-            console.log(response);
-            res.end(JSON.stringify(response));
-        });
-    });
-})
-
-var server = app.listen(8081, function () {
-
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("sever is live : http://%s:%s", host, port)
-
-})
+app.use('/upload', function (req, res, next) {
+    var now = Date.now();
+    jqupload.fileHandler({
+        uploadDir: function () {
+            return __dirname + '/public/uploads/' + now;
+        },
+        uploadUrl: function () {
+            return '/uploads/' + now;
+        }
+    })(req, res, next);
+});
